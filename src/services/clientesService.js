38fa103545c6e834fed3e13ef7aef7de240543f2
vlentinLZN . <diegovalentinlozano@gmail.com>
2026-09-12 20:19:50 -0300

    feat(services): incorporar soporte de AbortSignal en clientesService

--- src/services/clientesService.js
+++ src/services/clientesService.js
@@ -2,13 +2,12 @@ import axios from "axios";
 
 const URL = "https://fakestoreapi.com/users";
 
-const obtenerClientes = async () => {
-    const respuesta = await axios.get(URL);
+const obtenerClientes = async (signal) => {
+    const respuesta = await axios.get(URL, { signal });
     return respuesta.data;
 };
 
 const crearCliente = async (cliente) => {
-
     const respuesta = await axios.post(
         URL,
         cliente
